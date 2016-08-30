db28f8fe8474b141a448786d12ddb2bb3c83f5b0
Aleksandar Bukva <aleksandar.bukva@yandex.com>
2016-08-30 14:49:48 +0200

    Added option to choose between first and second oreder

--- src/picongpu/include/fields/MaxwellSolver/DirSplitting/DirSplitting.hpp
+++ src/picongpu/include/fields/MaxwellSolver/DirSplitting/DirSplitting.hpp
@@ -99,7 +99,7 @@ class DirSplitting : private ConditionCheck<fieldSolver::FieldSolver>
 {
 private:
     template<typename SpaceTwist, typename OrientationTwist,typename JSpaceTwist,typename CursorE, typename CursorB, typename CursorJ, typename GridSize>
-    void propagate(CursorE cursorE, CursorB cursorB,CursorJ cursorJ,CursorE old_cursorE, CursorB old_cursorB, GridSize gridSize, const float_X timeFactor) const
+    void propagate(CursorE cursorE, CursorB cursorB,CursorJ cursorJ,CursorE old_cursorE, CursorB old_cursorB, GridSize gridSize, const float_X timeFactor, const float_X addCurrent) const
     {
         using namespace cursor::tools;
         using namespace PMacc::math;
@@ -141,7 +141,7 @@ private:
                 cursor::make_NestedCursor(twistVectorForDirSplitting<SpaceTwistSimDim, OrientationTwist>(cursorJ)),
                 cursor::make_NestedCursor(twistVectorForDirSplitting<SpaceTwistSimDim, OrientationTwist>(old_cursorE)),
                 cursor::make_NestedCursor(twistVectorForDirSplitting<SpaceTwistSimDim, OrientationTwist>(old_cursorB)),
-                DirSplittingKernel<BlockDim,JSpaceTwist>((int)gridSizeTwisted.x(), timeFactor));
+                DirSplittingKernel<BlockDim,JSpaceTwist>((int)gridSizeTwisted.x(), timeFactor, addCurrent));
     }
 
 
@@ -260,7 +260,7 @@ public:
                   fieldJ_coreBorder.origin(),
                   old_fieldE_coreBorder.origin(),
                   old_fieldB_coreBorder.origin(),
-                  gridSize, float_X(0.5));
+                  gridSize, float_X(1.0), float_X(1.0));
 
 #if (SENTOKU==1) //prpagateX adds current
         propagateX<Space_XJ,Orientation_XJ, 1>(
@@ -283,7 +283,7 @@ public:
                   fieldJ_coreBorder.origin(),
                   old_fieldE_coreBorder.origin(),
                   old_fieldB_coreBorder.origin(),
-                  gridSize, float_X(1.0));
+                  gridSize, float_X(1.0), float_X(1.0));
 #if (SENTOKU==1)
         propagateX<Space_XJ,Orientation_XJ, 0>(
                   fieldE_coreBorder.origin(),
@@ -311,16 +311,18 @@ public:
         __setTransactionEvent(fieldB.asyncCommunication(__getTransactionEvent()));
 #endif
 
+#if 0
 	    propagate<Space_X,Orientation_X,JDir_X>(
 			    fieldE_coreBorder.origin(),
 			    fieldB_coreBorder.origin(),
 			    fieldJ_coreBorder.origin(),
 			    old_fieldE_coreBorder.origin(),
 			    old_fieldB_coreBorder.origin(),
-			    gridSize, float_X(0.5));
+			    gridSize, float_X(0.5), float_X(2.0));
 
 	    __setTransactionEvent(fieldE.asyncCommunication(__getTransactionEvent()));
 	    __setTransactionEvent(fieldB.asyncCommunication(__getTransactionEvent()));
+#endif
 
 #if (SIMDIM==DIM3)
         //! \todo: currently 3D: check this code if someone enable 3D
